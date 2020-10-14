ef1072a44be3af420ec86fd4784c19b550c6aad1
ehu <ehu@158.227.124.140>
2020-10-14 15:55:17 +0200

    proba10

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -4,7 +4,7 @@ public abstract class Card {
 //Git errepositoriako komentarioaaa
     public static final int TYPE_CHANCE = 1;
     public static final int TYPE_CC = 2;
-//asdf
+//asdfasdf
     public abstract String getLabel();
     public abstract void applyAction();
     public abstract int getCardType();
